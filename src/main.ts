e983efbc53db5e9e66f502fcf839d2d2d5a3fd01
SuiltaPico <2082183663@qq.com>
2023-08-09 17:17:44 +0800

    update mdi

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import { Quasar } from "quasar";
 import quasarLang from "quasar/lang/zh-CN";
-import quasarIconSet from "quasar/icon-set/mdi-v6";
+import quasarIconSet from "quasar/icon-set/mdi-v7";
 import "./styles/color"
 
 // Import icon libraries
-import "@quasar/extras/mdi-v6/mdi-v6.css";
+import "@quasar/extras/mdi-v7/mdi-v7.css";
 
 // A few examples for animations from Animate.css:
 // import @quasar/extras/animate/fadeIn.css
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 
 import vue from "@vitejs/plugin-vue";
+import vue_jsx from "@vitejs/plugin-vue-jsx";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
-
 
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
@@ -12,6 +12,7 @@ import tailwindcss from "tailwindcss";
 export default defineConfig({
   plugins: [
     vue({ template: { transformAssetUrls } }),
+    vue_jsx(),
     quasar({
       sassVariables: "src/styles/quasar-variables.sass",
     }),
